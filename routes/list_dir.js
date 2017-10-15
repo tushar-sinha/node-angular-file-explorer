70f9cc5cfecf53a1dc65c6e07eec81a797ddf06e
Tushar Sinha <tushar.sinha313@gmail.com>
2017-10-16 00:02:24 +0530

    removed sync block - using callback now

--- routes/list_dir.js
+++ routes/list_dir.js
@@ -6,39 +6,46 @@ var fs = require('fs');
 var _ = require('underscore');
 
 router.get('/', function(req, res) {
- var currentDir =  dir;
- var query = req.query.path || '';
- if (query) currentDir = path.join(dir, query);
- console.log("browsing ", currentDir);
- fs.exists(currentDir, function (exists) {
-      if(exists){
-          fs.readdir(currentDir, function (err, files) {
-              if (err) {
-                 throw err;
-               }
-               var data = [];
-               data.push({root: dir, isRoot:true});
-               files
-               .forEach(function (file) {
-                 try {
-                         var isDirectory = fs.statSync(path.join(currentDir,file)).isDirectory();
-                         if (isDirectory) {
-                           data.push({ type : 'folder', IsDirectory: true, Path : path.join(query, file), Name : file, size : 0, isRoot:false});
-                         } else {
-                           var ext = path.extname(file);
-                           data.push({ type : 'file', Name : file, Ext : ext, IsDirectory: false, Path : path.join(currentDir, file), size : fs.statSync(path.join(currentDir,file)).size, isRoot:false});
-                         }
-                 } catch(e) {
-                   console.log(e);
-                 }
-               });
-               data = _.sortBy(data, function(f) { return f.type });
-               res.json(data);
-           });
-      }
-      else{
-        res.end('File does not exists');
-      }
-    });
+  var currentDir =  dir;
+  var query = req.query.path || '';
+  if (query) currentDir = path.join(dir, query);
+  fs.exists(currentDir, function (exists) {
+    if(exists){
+      fs.readdir(currentDir, function (err, files) {
+        if (err) {
+          throw err;
+        }
+        var data = [];
+        data.push({root: dir, isRoot:true});
+        var counter = 0;
+        files.forEach(function (file) {
+          try {
+            //var isDirectory = fs.statSync(path.join(currentDir,file)).isDirectory();
+            fs.stat(path.join(currentDir,file), function(err, stats){
+              if(err){ throw err; }
+              else{
+                if(stats.isDirectory()) {
+                  data.push({ type : 'folder', IsDirectory: true, Path : path.join(query, file), Name : file, size : 0, isRoot:false});
+                } else {
+                  var ext = path.extname(file);
+                  data.push({ type : 'file', Name : file, Ext : ext, IsDirectory: false, Path : path.join(currentDir, file), size : stats.size, isRoot:false});
+                }
+                counter++;
+                if(counter == files.length){
+                data = _.sortBy(data, function(f) { return f.type });
+                res.json(data);
+                }
+              }
+            });
+          } catch(e) {
+            console.log(e);
+          }
+        });
+      });
+    }
+    else{
+      res.end('File does not exists');
+    }
+  });
 });
 module.exports = router;
